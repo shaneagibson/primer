5f0b62c5a47dd5d1b076e219600446710267f6a3
Shane Gibson <shane.a.gibson@gmail.com>
2013-11-28 15:53:38 +0000

    Added javadoc

--- src/main/java/uk/co/epsilontechnologies/primer/matcher/MapMatcher.java
+++ src/main/java/uk/co/epsilontechnologies/primer/matcher/MapMatcher.java
@@ -10,16 +10,32 @@ import java.util.Map;
  */
 public class MapMatcher implements Matcher<Map<String,String>,Map<String,String>> {
 
+    /**
+     * The string matcher to use when comparing values of the map
+     */
     private final StringMatcher stringMatcher;
 
+    /**
+     * Constructs the map matcher using a String Matcher for comparison of values
+     */
     MapMatcher() {
         this(new StringMatcher());
     }
 
+    /**
+     * Constructs the map matcher using the given String Matcher for comparison of values
+     * @param stringMatcher
+     */
     MapMatcher(final StringMatcher stringMatcher) {
         this.stringMatcher = stringMatcher;
     }
 
+    /**
+     * Matches the primed map against the request map
+     * @param primedMap the primed map to match against
+     * @param requestMap the request map to match
+     * @return true if the request map contains all of the primed map keys and the corresponding values are equivalent, false otherwise
+     */
     @Override
     public boolean match(final Map<String, String> primedMap, final Map<String, String> requestMap) {
         for (final String primedKey : primedMap.keySet()) {
--- src/main/java/uk/co/epsilontechnologies/primer/matcher/Matcher.java
+++ src/main/java/uk/co/epsilontechnologies/primer/matcher/Matcher.java
@@ -7,6 +7,12 @@ package uk.co.epsilontechnologies.primer.matcher;
  */
 public interface Matcher<T,K> {
 
+    /**
+     * Matches the primed value against the request value
+     * @param primedValue the primed value to match against
+     * @param requestValue the request value to match
+     * @return true if the values match, false otherwise
+     */
     boolean match(T primedValue, K requestValue);
 
 }
